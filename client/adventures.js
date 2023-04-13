45318514c24266cf9c31ca3d64abc91f5331e2e6
Ethan Dale <ethan.dale.147@gmail.com>
2023-04-12 18:02:04 -0600

    second commit

---
+++ client/adventures.js
@@ -0,0 +1,12 @@
+document.querySelector('#bells').addEventListener('click', () =>{
+    window.location.href = "./myHikes.html";
+
+})
+document.querySelector('#dimple').addEventListener('click', () =>{
+   
+
+})
+document.querySelector('#blanche').addEventListener('click', () =>{
+   
+
+})
